bab01315f509316934cb01c18e516f96dea9145d
Miles Murphy <themurf100@gmail.com>
2023-11-22 22:57:54 -0500

    did more work on login/register system

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Signin, Signup
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class SigninForm(forms.ModelForm):
@@ -12,3 +14,13 @@ class SignupForm(forms.ModelForm):
     class Meta:
         model = Signup
         fields = '__all__'
+
+
+# This form is not currently used, but I included in case you need to design a custom UserRegistrationForm where
+# you can ask the user to enter email and password instead of password and username to register your site
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
