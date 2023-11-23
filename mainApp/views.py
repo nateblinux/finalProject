d43c874cc5fb4da0c8b39f54767526dfb4318f96
Miles Murphy <themurf100@gmail.com>
2023-11-22 22:31:53 -0500

    did more work on login/register system

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import SigninForm, SignupForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
 
+    return render(request, 'accounts/login.html', {'form': form})
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
