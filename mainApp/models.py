fac9256f5db2eecc4819254cff3cfdec13e9cc8b
Miles Murphy <themurf100@gmail.com>
2023-11-22 20:27:11 -0500

    Added ticketmaster.html

--- mainApp/models.py
+++ mainApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Signin(models.Model):
     email = models.EmailField()
 
+
 class Signup(models.Model):
     firstName = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
