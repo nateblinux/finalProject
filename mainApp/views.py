eaef44054f8f7a772a32c633ce39e27581f98deb
Miles Murphy <themurf100@gmail.com>
2023-11-27 19:48:37 -0500

    made more progress on user system

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,12 +2,11 @@ import traceback
 from encodings import undefined
 
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import SigninForm, SignupForm
-from django.contrib.auth import logout, login
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
 import requests
 import datetime
 import json
@@ -149,19 +148,51 @@ def home(request):
 
 
 def signin(request):
-    form = SigninForm()
-    context = {'form': form}
-    return render(request, 'authentication/sign-in.html', context)
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect('home')
+        return redirect('signin')
+    return render(request, 'authentication/sign-in.html')
 
 
 def signup(request):
-    form = SignupForm()
-    context = {'form': form}
-    return render(request, 'authentication/sign-up.html', context)
+
+    if request.method == 'POST':
+
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been successfully created.")
+
+        return redirect('signin')
+
+    return render(request, 'authentication/sign-up.html')
 
 
 def signout(request):
-    form = SignupForm()
-    context = {'form': form}
-    return render(request, 'authentication/sign-out.html', context)
+    logout(request)
+    messages.success(request, "Successfully Logged Out")
+    return redirect('home')
 
